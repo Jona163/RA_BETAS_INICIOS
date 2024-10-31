a65bd1f9e7161e5de564a3ad2c37f4e9abddc2ce
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-31 10:39:57 -0600

    Update rq2_test.py

--- rq2_test.py
+++ rq2_test.py
@@ -4,3 +4,17 @@ import qrcode
 from matplotlib import pyplot as plt
 import cv2.aruco as aruco
 
+
+# Función para generar un código QR
+def generate_qr(image_filename, qr_filename):
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(image_filename)
+    qr.make(fit=True)
+    qr_image = qr.make_image(fill_color="black", back_color="white")
+    qr_image.save(qr_filename)
+    print(f"Código QR guardado como: {qr_filename}")
+
+# Generar el código QR para la imagen que deseas mostrar en 3D
+image_filename = "bellaa.png"  # Cambia a la imagen que quieres mostrar
+qr_filename = "qr_code.png"
+generate_qr(image_filename, qr_filename)
