e7c0f1426efe24308adcca25c5f51bd182e3c1b7
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-31 10:34:38 -0600

    Update prueba_dicc_aruco.py

--- prueba_dicc_aruco.py
+++ prueba_dicc_aruco.py
@@ -1,1 +1,6 @@
+import cv2
+import cv2.aruco as aruco  # Asegúrate de importar aruco explícitamente
+
+# Cargar el diccionario de ArUco
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
 
