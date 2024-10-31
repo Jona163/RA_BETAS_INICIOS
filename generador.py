18b20cdd1c28ef3549b0ada572f34c7cc10e875f
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-31 10:41:40 -0600

    Update generador.py

--- generador.py
+++ generador.py
@@ -1,1 +1,8 @@
 
+import cv2
+import numpy as np
+
+# Configura el diccionario de ArUco y el ID del marcador
+aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+marker_id = 33  # Puedes cambiar este ID (valores entre 0 y el tamaño del diccionario -1)
+marker_size = 200  # Tamaño del marcador en píxeles
