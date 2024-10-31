02af965ed4e523ba05d4d90badd1c8feb8c18c0a
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-31 10:33:55 -0600

    Update Prueba_paquete_aruco.py

--- Prueba_paquete_aruco.py
+++ Prueba_paquete_aruco.py
@@ -1,1 +1,10 @@
+import cv2
+import cv2.aruco as aruco
+
+# Verifica si detectMarkers está disponible
+try:
+    aruco.detectMarkers  # Solo para verificar la existencia
+    print("detectMarkers está disponible.")
+except AttributeError:
+    print("detectMarkers NO está disponible.")
 
