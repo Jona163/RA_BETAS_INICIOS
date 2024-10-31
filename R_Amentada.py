1da9fe6af7526e2b1659f6813453cf12500ae1ba
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-31 10:35:25 -0600

    Update R_Amentada.py

--- R_Amentada.py
+++ R_Amentada.py
@@ -9,3 +9,14 @@ cap = cv2.VideoCapture(0)
 # Cargar el diccionario de ArUco
 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
 parameters = aruco.DetectorParameters()  # Cambia esto a DetectorParameters()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Detectar marcadores en el frame
+    corners, ids, rejected = aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
+    if ids is not None:
+        # Dibuja los contornos de los marcadores
+        aruco.drawDetectedMarkers(frame, corners, ids)
