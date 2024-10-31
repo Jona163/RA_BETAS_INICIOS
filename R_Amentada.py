999d146f579935d152dd44f9ae477e107f4f2c60
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-31 10:35:16 -0600

    Update R_Amentada.py

--- R_Amentada.py
+++ R_Amentada.py
@@ -1,1 +1,11 @@
+import cv2
+import numpy as np
+import cv2.aruco as aruco
 
+aruco = cv2.aruco
+# Inicializa la cámara
+cap = cv2.VideoCapture(0)
+
+# Cargar el diccionario de ArUco
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+parameters = aruco.DetectorParameters()  # Cambia esto a DetectorParameters()
