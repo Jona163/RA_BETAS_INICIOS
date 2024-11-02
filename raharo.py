05d45aafbf844457606f7086ba7c52f3e6a718ec
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-02 13:00:04 -0600

    Update raharo.py

--- raharo.py
+++ raharo.py
@@ -5,3 +5,10 @@ import cv2.aruco as aruco
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+
+# Inicializa la cámara
+cap = cv2.VideoCapture(0)
+
+# Cargar el diccionario de ArUco
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+parameters = aruco.DetectorParameters()
