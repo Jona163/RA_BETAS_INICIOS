ae4b2c74afd30d04152adf200cde5fb13f0b48c9
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-02 13:00:13 -0600

    Update raharo.py

--- raharo.py
+++ raharo.py
@@ -12,3 +12,7 @@ cap = cv2.VideoCapture(0)
 # Cargar el diccionario de ArUco
 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
 parameters = aruco.DetectorParameters()
+
+# Cargar la imagen que quieres mostrar
+image_to_render = cv2.imread('ruta/a/tu/imagen.png')  # Cambia esto a la ruta de tu imagen
+image_to_render = cv2.flip(image_to_render, 0)  # Voltear la imagen para OpenGL
