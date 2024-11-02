57ab71cb40c64956693d3f8c3af5e231b90d0059
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-02 12:55:52 -0600

    Update qr_gen2.py

--- qr_gen2.py
+++ qr_gen2.py
@@ -3,3 +3,12 @@ import numpy as np
 import cv2.aruco as aruco
 import qrcode
 from matplotlib import pyplot as plt
+
+# Función para generar un código QR
+def generate_qr(image_filename, qr_filename):
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(image_filename)
+    qr.make(fit=True)
+    qr_image = qr.make_image(fill_color="black", back_color="white")
+    qr_image.save(qr_filename)
+    print(f"Código QR guardado como: {qr_filename}")
