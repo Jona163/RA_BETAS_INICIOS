dc7e5670440c3f0a921f37aaf5d899b0d147880a
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-03 16:04:35 -0600

    Update ra2_test.py

--- ra2_test.py
+++ ra2_test.py
@@ -3,5 +3,8 @@ import numpy as np
 import qrcode
 from matplotlib import pyplot as plt
 import cv2.aruco as aruco
+
 # Función para generar un código QR
 def generate_qr(image_filename, qr_filename):
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(image_filename)
