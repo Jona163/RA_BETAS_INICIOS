f9e6b85cdd14e4943119c907bdc08db96a763678
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-03 16:04:45 -0600

    Update ra2_test.py

--- ra2_test.py
+++ ra2_test.py
@@ -8,3 +8,5 @@ import cv2.aruco as aruco
 def generate_qr(image_filename, qr_filename):
     qr = qrcode.QRCode(version=1, box_size=10, border=5)
     qr.add_data(image_filename)
+    qr.make(fit=True)
+    qr_image = qr.make_image(fill_color="black", back_color="white")
